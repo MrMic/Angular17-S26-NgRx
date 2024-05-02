478e07558bbfbda00e4ec5b0a32c3f287ef8e70b
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-05-02 09:32:18 +0200

    🛠  Attaching Data to Actions

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -3,9 +3,10 @@ import { increment } from './counter.actions';
 
 const initialState = 0;
 
-export const counterReducer = createReducer(initialState, on(
-  increment, (state) => state + 1
-));
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state, action) => state + action.value),
+);
 
 // export function counterReducer(state = initialState) {
 //   return state;
