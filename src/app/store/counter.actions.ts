66f3081ecd96c18f96e761ece6490aa9729c7b18
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-05-03 09:59:05 +0200

    ⚒  Alternative way of defining Actions

--- src/app/store/counter.actions.ts
+++ src/app/store/counter.actions.ts
@@ -1,5 +1,21 @@
-import { createAction, props } from '@ngrx/store';
+import { Action, createAction, props } from '@ngrx/store';
 
-export const increment = createAction('[Counter] Increment', props<{
-  value: number;
-}>());
+// NOTE: ══════════════════════════ 1. First SOLUTION ══════════════════════════
+export const increment = createAction(
+  '[Counter] Increment',
+  props<{
+    value: number;
+  }>(),
+);
+
+// NOTE: ══════════════════════════ 2. Second SOLUTION ══════════════════════════
+// export const INCREMENT = '[Counter] Increment';
+
+// ______________________________________________________________________
+// export class IncrementAction implements Action {
+//   readonly type = INCREMENT;
+//
+//   constructor(public value: number) { }
+// }
+//
+// export type CounterActions = IncrementAction;
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,28 @@
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as Sentry from '@sentry/angular-ivy';
 
 import { AppModule } from './app/app.module';
 
+Sentry.init({
+  dsn: 'https://be088d305dfb573350916a655714f9f2@o4506586489815040.ingest.us.sentry.io/4507187428786176',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+enableProdMode();
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then((success) => console.log('Bootstrap success'))
+  .catch((err) => console.error(err));
