e8ef7876cc4b5d40e1351206ebd0149645f4c7a0
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-05-06 10:22:35 +0200

    🎉 feat(selector): Use of Selector

---
+++ src/app/store/counter.selector.ts
@@ -0,0 +1,5 @@
+import { createSelector } from "@ngrx/store";
+
+export const selectCount = (state: { counter: number }) => state.counter;
+// export const selectDoubleCount = (state: { counter: number }) => state.counter * 2;
+export const selectDoubleCount = createSelector(selectCount, (state: number) => state * 2);
