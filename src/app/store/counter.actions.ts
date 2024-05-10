1aa7ec2d8accb664ae14afafb776c4bdbb5f3b68
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-05-10 10:22:22 +0200

    ✨ feat: Add a second Effect

--- src/app/store/counter.actions.ts
+++ src/app/store/counter.actions.ts
@@ -1,18 +1,23 @@
 import { createAction, props } from '@ngrx/store';
 
+// ______________________________________________________________________
+export const init = createAction('[Counter] Init');
+
+export const set = createAction('[Counter] Set', props<{ value: number }>());
+
 // NOTE: ══════════════════════════ 1. First SOLUTION ══════════════════════════
 export const increment = createAction(
-  '[Counter] Increment',
-  props<{
-    value: number;
-  }>(),
+'[Counter] Increment',
+props<{
+value: number;
+}>(),
 );
 
 export const decrement = createAction(
-  '[Counter] Decrement',
-  props<{
-    value: number;
-  }>(),
+'[Counter] Decrement',
+props<{
+value: number;
+}>(),
 );
 
 // NOTE: ══════════════════════════ 2. Second SOLUTION ══════════════════════════
